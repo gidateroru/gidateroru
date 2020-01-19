46fc7065fa328b3d2fe912c598a614be2874ad72
Gıda Terörü <gidateroru@gmail.com>
2020-01-19 21:56:27 +0000

    ...

--- _site/gitp.sh
+++ _site/gitp.sh
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
-git init
+git config --global user.email "gidateroru@gmail.com"
+git config --global user.name "Gıda Terörü"
+git remote add origin https://github.com/gidateroru/gidateroru.git
 git status
-=======
-git status
-git add .
+git add  --all :/
 git commit -m "..."
+git pull origin master --rebase
 git push origin master -u
->>>>>>> ...
